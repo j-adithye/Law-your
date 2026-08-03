d0542b25f9a2e4e404e3a78f513f7536d66ecc43
J-Adithye <adithyej8@gmail.com>
2026-08-03 23:24:22 +0530

    now works in all numbers

--- extractor.py
+++ extractor.py
@@ -1,7 +1,8 @@
 import pdfplumber as plumber
 import re
+from pprint import pprint
 
-def chunk_text(full_text):
+def chunk_text(full_text):  #inputs 1 page
 	section_pattern = re.compile(r'^(\d+)\.\s+[A-Z]')
 
 	lines = []
@@ -9,27 +10,35 @@ def chunk_text(full_text):
 	chunk = []
 
 	for idx in range(len(full_text)):
-			lines.append(full_text[idx]['text'])
+			lines.append(full_text[idx]['text']) #breaks it into lines
 	i = 0
 	while i != len(lines):
-		if section_pattern.match(lines[i]):
+		if section_pattern.match(lines[i]):  #use pattern '1. A' to chunk text
 			if not chunk:
 				chunk.append(lines[i])
 				i+=1
 			else:
-				chunks[chunk[0][0:2]] = chunk
+				idx_of_dict = ''
+				for n in chunk[0][0:3]:
+					if n.isdigit():
+						idx_of_dict += n 
+				chunks[idx_of_dict] = chunk
 				chunk = []
 				chunk.append(lines[i])
 				i+=1
 		else:
 			chunk.append(lines[i])
 			i+=1
-	chunks[chunk[0][0:2]] = chunk
-	print(chunks)
+	idx_of_dict = ''
+	for n in chunk[0][0:3]:
+		if n.isdigit():
+			idx_of_dict += n 
+	chunks[idx_of_dict] = chunk
+	pprint(chunks, indent=4)
 
 with plumber.open("./data/raw/bns.pdf") as pdf:
 	for i in range(1):
-		page = pdf.pages[27-1]
+		page = pdf.pages[16-1]
 		height = page.height
 		width = page.width
 		cropped = page.crop((0,0,width,height - 65)) #left,up,right,down
