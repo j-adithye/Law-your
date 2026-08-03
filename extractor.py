ca41a91e7d57af3ffc014f6c0c844a86dd3c87ec
J-Adithye <adithyej8@gmail.com>
2026-08-03 20:46:52 +0530

    chunking of extracted text based on pattern

--- extractor.py
+++ extractor.py
@@ -1,11 +1,47 @@
 import pdfplumber as plumber
+import re
+
+def chunk_text(full_text):
+	section_pattern = re.compile(r'^(\d+)\.\s+[A-Z]')
+
+	lines = []
+	chunks = {}
+	chunk = []
+
+	for idx in range(len(full_text)):
+			lines.append(full_text[idx]['text'])
+
+	i = 0
+	while i != len(lines):
+		if section_pattern.match(lines[i]):
+			if not chunk:
+				chunk.append(lines[i])
+				i+=1
+			else:
+				chunks[chunk[0][0:2]] = chunk
+				chunk = []
+				chunk.append(lines[i])
+				i+=1
+		else:
+			chunk.append(lines[i])
+			i+=1
+	print(chunks)
 
 with plumber.open("./data/raw/bns.pdf") as pdf:
 	for i in range(1):
-		page = pdf.pages[16-1]
+		page = pdf.pages[27-1]
 		height = page.height
 		width = page.width
 		cropped = page.crop((0,0,width,height - 65)) #left,up,right,down
-		for i in cropped.extract_text_lines():
-			print(i["text"])
-		# print(cropped.extract_text_lines()[1]['text'])
+
+		full_text = cropped.extract_text_lines()
+		# lines = []
+
+		# for idx in range(len(full_text)):
+		# 	lines.append(full_text[idx]['text'])
+		# section_pattern = re.compile(r'^(\d+)\.\s+[A-Z]')
+		# for line in lines:
+		# 	if section_pattern.match(line):
+		# 				print(line[0:2])
+		# print("##########################################")
+		chunk_text(full_text)
